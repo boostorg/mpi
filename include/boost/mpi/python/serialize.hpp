947d17157304ceb93926ab961487e9c86fc7c2d2
Jonathan Wakely <github@kayari.org>
2017-02-23 02:19:45 +0000

    Add header for serialization::make_array (#40)

--- include/boost/mpi/python/serialize.hpp
+++ include/boost/mpi/python/serialize.hpp
@@ -35,6 +35,7 @@
 
 #include <boost/serialization/split_free.hpp>
 #include <boost/serialization/array.hpp>
+#include <boost/serialization/array_wrapper.hpp>
 
 #include <boost/smart_ptr/scoped_array.hpp>
 
