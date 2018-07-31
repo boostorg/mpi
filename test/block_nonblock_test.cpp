8a6f56413cf1308f794786e3bdfd8694d51e5ee7
Alain Miniussi <alain.miniussi@oca.eu>
2018-07-31 14:11:27 +0200

    Integrate user test in bjam system. refs #62

--- test/block_nonblock_test.cpp
+++ test/block_nonblock_test.cpp
@@ -6,33 +6,46 @@
 
 namespace mpi = boost::mpi;
 
+bool test(mpi::communicator const& comm, std::vector<int> const& ref, bool iswap)
+{
+  int rank = comm.rank();
+  
+  if (rank == 0) {
+    std::vector<int> data;
+    if (iswap) {
+      auto req = comm.irecv(1, 0, data);
+      req.wait();
+    } else {
+      comm.recv(1, 0, data);
+    }
+    std::cout << "Process 0 received:" << std::endl;
+    std::copy(std::begin(data), std::end(data), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "While expecting:" << std::endl;
+    std::copy(std::begin(ref), std::end(ref), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    return (data == ref);
+  } else if (rank == 1) {
+    std::vector<int> vec = ref;
+    if (iswap) {
+      comm.send(0, 0, vec);
+    } else {
+      auto req = comm.isend(0, 0, vec);
+      req.wait();
+    }
+    return true;
+  } 
+}
+
 int main(int argc, char **argv)
 {
-    bool iswap = argc > 1 && argv[1] == std::string("iswap");
-
-    mpi::environment env(argc, argv);
-    mpi::communicator comm_world;
-    auto rank = comm_world.rank();
-
-    if (rank == 0) {
-        std::vector<int> data;
-        if (iswap) {
-            auto req = comm_world.irecv(1, 0, data);
-            req.wait();
-        } else {
-            comm_world.recv(1, 0, data);
-        }
-        std::cout << "Process 0 received:" << std::endl;
-        std::copy(std::begin(data), std::end(data), std::ostream_iterator<int>(std::cout, " "));
-        std::cout << std::endl;
-
-    } else if (rank == 1) {
-        std::vector<int> vec = {1, 2, 3, 4, 5};
-        if (iswap) {
-            comm_world.send(0, 0, vec);
-        } else {
-            auto req = comm_world.isend(0, 0, vec);
-            req.wait();
-        }
-    } 
+  mpi::environment env(argc, argv);
+  mpi::communicator world;
+  std::vector<int> ref(13); // don't assume we're lucky
+  for(int i = 0; i < int(ref.size()); ++i) {
+    ref[i] = i;
+  }
+  bool send  = test(world, ref, true);
+  bool isend = test(world, ref, false);
+  return send && isend ? 0 : 1;
 }
